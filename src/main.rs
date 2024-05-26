87cdf8f615f6d1c95f98c6211a8a75127ad9372f
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2024-05-26 14:45:43 +0300

    Added Program struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 trait Data<T> {
   fn read(&self) -> T;
   fn write(&mut self, number: T);
@@ -50,6 +52,16 @@ impl Signed for Register {
   }
 }
 
+impl fmt::Display for Register {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    if self.positive {
+      write!(f, "+{}", self.data)
+    } else {
+      write!(f, "-{}", self.data)
+    }
+  }
+}
+
 #[derive(Clone, Copy)]
 struct Word {
   data: u32,
@@ -57,7 +69,7 @@ struct Word {
 }
 
 impl Word {
-  const MASK: u32 = 0b1111_1111_1111_1111_1111_1111_1111_1111;
+  const MASK: u32 = 0b0111_1111_1111_1111_1111_1111_1111_1111;
 
   fn new(number: u32) -> Self {
     Self {
@@ -93,28 +105,39 @@ impl Signed for Word {
   }
 }
 
-enum Compare {
-  None,
-  Less,
-  Equal,
-  Greater,
+impl fmt::Display for Word {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "{}{:06} {:06} {:06} {:06} {:06}",
+      if self.positive { "+" } else { "-" },
+      self.data & 0b0011_1111,
+      self.data & 0b1111_1100_0000,
+      self.data & 0b0011_1111_0000_0000_0000,
+      self.data & 0b1111_1100_0000_0000_0000_0000,
+      self.data & 0b0011_1111_0000_0000_0000_0000_0000_0000,
+    )
+  }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct Instruction {
   command: u32,
   modifier: u32,
   index: u32,
   address: u32,
+  sign: u32,
 }
 
 impl Instruction {
   const COMMAND_MASK: u32 = 0b0000_0000_0011_1111;
-  const MODIFIER_MASK: u32 = 0b0000_0011_1100_0000;
-  const INDEX_MASK: u32 = 0b0011_1100_0000_0000;
-  const ADDRESS_MASK: u32 = 0b1111_1111_1111_1000_0000_0000_0000_0000;
+  const MODIFIER_MASK: u32 = 0b0000_1111_1100_0000;
+  const INDEX_MASK: u32 = 0b1111_1100_0000_0000;
+  const ADDRESS_MASK: u32 = 0b0011_1111_1111_1100_0000_0000_0000_0000;
 
-  fn new(command: u32, modifier: u32, index: u32, address: u32) -> Self {
+  fn new(command: u32, modifier: u32, index: u32, address: u32, sign: u32) -> Self {
     Self {
+      sign,
       command,
       modifier,
       index,
@@ -127,6 +150,7 @@ impl Instruction {
       | ((self.modifier & 0b111111) << 6)
       | ((self.index & 0b111111) << 12)
       | ((self.address & 0b1111111111111) << 18)
+      | (self.sign << 31)
   }
 
   fn unpack(word: u32) -> Self {
@@ -135,8 +159,33 @@ impl Instruction {
       modifier: (word & Self::MODIFIER_MASK) >> 6,
       index: (word & Self::INDEX_MASK) >> 12,
       address: (word & Self::ADDRESS_MASK) >> 18,
+      sign: (word >> 31) & 1
     }
   }
+}
+
+struct Program {
+  instructions: Vec<Instruction>
+}
+
+impl Program {
+  fn new() -> Self {
+    Self {
+      instructions: Vec::new()
+    }
+  }
+
+  fn add(&mut self, instruction: Instruction) {
+    self.instructions.push(instruction);
+  }
+}
+
+#[derive(Debug)]
+enum Compare {
+  None,
+  Less,
+  Equal,
+  Greater,
 }
 
 struct Computer {
@@ -169,10 +218,70 @@ impl Computer {
       i6: Register::default(),
     }
   }
+
+  fn load(&mut self, program: Program) {
+    for (index, instruction) in program.instructions.iter().enumerate() {
+      self.memory[index] = Word::new(instruction.pack());
+    }
+  }
+
+  fn execute(&mut self) {
+    for (index, word) in self.memory.iter().enumerate() {
+      let instruction = Instruction::unpack(word.read());
+
+      match instruction.command {
+        0 => {
+          // NOOP
+          continue
+        },
+        8 => {
+          // LDA
+          self.a = Word::new(instruction.modifier)
+        },
+        15 => {
+          // LDX
+          self.x = Word::new(instruction.modifier)
+        },
+        _ => unimplemented!("Unknown command")
+      }
+    }
+  }
+}
+
+impl fmt::Display for Computer {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    writeln!(f, "Memory:")?;
+    for (i, word) in self.memory.iter().enumerate().rev() {
+      write!(f, "{:04X}: ", i)?;
+      writeln!(f, "{}", word)?;
+    }
+
+    writeln!(f, "Overflow: {}", self.overflow)?;
+    writeln!(f, "Comparison: {:?}", self.comparison)?;
+    writeln!(f, "A: {}", self.a)?;
+    writeln!(f, "X: {}", self.x)?;
+    writeln!(f, "I1: {}", self.i1)?;
+    writeln!(f, "I2: {}", self.i2)?;
+    writeln!(f, "I3: {}", self.i3)?;
+    writeln!(f, "I4: {}", self.i4)?;
+    writeln!(f, "I5: {}", self.i5)?;
+    write!(f, "I6: {}", self.i6)
+  }
 }
 
 fn main() {
-  let computer = Computer::new();
+  let mut computer = Computer::new();
+  let mut program = Program::new();
+  program.add(Instruction::new(8, 3, 2, 0, 1));
+  program.add(Instruction::new(8, 11, 2, 0, 1));
+  program.add(Instruction::new(8, 11, 0, 0, 1));
+  program.add(Instruction::new(8, 5, 0, 0, 1));
+  program.add(Instruction::new(8, 5, 4, 0, 0));
+
+  computer.load(program);
+  println!("{}", computer);
+
+  computer.execute();
 }
 
 #[cfg(test)]
